61aa63ef33fcce9d066f22941c0f3624a9a8ec13
Tom Lee <win981026@gmail.com>
2019-05-20 11:03:27 -0700

    db: rename "user" table to usr

--- app/auth.py
+++ app/auth.py
@@ -18,7 +18,7 @@ def load_logged_in_user():
         g.user = None
     else:
         db, cur = get_db()
-        cur.execute('SELECT * FROM "user" WHERE id = (%s);', (user_id,))
+        cur.execute('SELECT * FROM usr WHERE id = (%s);', (user_id,))
         g.user = cur.fetchone()
 
 @bp.route('/register', methods=('GET', 'POST'))
@@ -34,13 +34,13 @@ def register():
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        cur.execute('SELECT id FROM "user" WHERE username = (%s);', (username,))
+        cur.execute('SELECT id FROM usr WHERE username = (%s);', (username,))
         if cur.fetchone() is not None:
             error = 'User {} is already registered.'.format(username)
 
         if error is None:
             cur.execute(
-                'INSERT INTO "user" (username, password) VALUES (%s, %s);',
+                'INSERT INTO usr (username, password) VALUES (%s, %s);',
                 (username, generate_password_hash(password))
             )
             db.commit()
@@ -57,7 +57,7 @@ def login():
         db, cur = get_db()
         error = None
 
-        cur.execute('SELECT * FROM "user" WHERE username = (%s);', (username,))
+        cur.execute('SELECT * FROM usr WHERE username = (%s);', (username,))
         user = cur.fetchone()
 
         if user is None:
@@ -68,7 +68,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('auth.login')) # TODO: redirect to index
+            return redirect(url_for('index'))
 
         flash(error)
 
@@ -78,7 +78,7 @@ def login():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('auth.login')) # TODO: redirect to index
+    return redirect(url_for('index'))
 
 
 def login_required(view):
--- sql/seed.sql
+++ sql/seed.sql
@@ -1,3 +1,6 @@
-INSERT INTO "user" (id, username, password) VALUES (DEFAULT, 'leesw98', 'supersecurehashedpassword');
+INSERT INTO usr (username, password)
+VALUES
+  ('test', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
+  ('other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79');
 
-SELECT * FROM "user";
+SELECT * FROM usr;
